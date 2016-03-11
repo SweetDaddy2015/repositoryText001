366e8ca120f54067be783a2116f11d16c66e15bf
H <viphank@sina.com>
2016-03-11 19:14:58 +0800

    初始化代码

---
+++ QQ/QQ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQ
+//
+//  Created by H on 16/3/11.
+//  Copyright © 2016年 TanZhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QQ/QQ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QQ
+//
+//  Created by H on 16/3/11.
+//  Copyright © 2016年 TanZhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
